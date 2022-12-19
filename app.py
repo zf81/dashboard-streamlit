7bfbc7bad6940fe64b245d0e65a0470732999f45
zf81 <111600840+zf81@users.noreply.github.com>
2022-12-18 23:53:22 -0500

    Update

--- app.py
+++ app.py
@@ -4,45 +4,82 @@ import pandas as pd
 import numpy as np
 
 
-df = pd.read_csv('https://raw.githubusercontent.com/zf81/dashboard-streamlit/main/data/Stars.csv')
-
-# Fix column names 
-df.columns = df.columns.str.replace('[^A-Za-z0-9]+', '_')
-df.columns = df.columns.str.lower()
-df.columns.values
-
-#### Put together dataframes ####
-df_temp = df.filter(['Temperature (K)','Luminosity(L/Lo)','Radius(R/Ro)'])
-# Get rid of irrelevant rows
-df_temp = df_temp[df_temp['Luminosity(L/Lo)'].apply(lambda x: str(x).isdigit())]
-df_temp = df_temp[df_temp['Radius(R/Ro)'].apply(lambda x: str(x).isdigit())]
-df_temp = df_temp[df_temp['Temperature (K)'].str.contains('Unknown')==False ]
-# Convert columns to integers
-df_temp['Luminosity(L/Lo)'] = df_temp['Luminosity(L/Lo)'].astype(int)
-df_temp['Radius(R/Ro)'] = df_temp['Radius(R/Ro)'].astype(int)
-# Group by temperature and get sum of each group
-df_temp = df_temp.groupby('Temperature (K)').sum()
-
-
-df_class = df.filter(['Spectral Class','Star color','Absolute magnitude(Mv)'])
-# Get rid of irrelevant rows
-df_class = df_class[df_class['Star color'].apply(lambda x: str(x).isdigit())]
-df_class = df_class[df_class['Absolute magnitude(Mv)'].apply(lambda x: str(x).isdigit())]
-# Convert columns to integers
-df_class['Star color'] = df_class['Star color'].astype(int)
-df_class['Absolute magnitude(Mv)'] = df_class['Absolute magnitude(Mv)'].astype(int)
-# Group by Spectral Class and get sum of each group
-df_class = df_class.groupby('Spectral Class').sum()
-
-#### Putting together streamlit dashboard ####
 st.title('Stars: Temperature (K), Luminosity(L/Lo), Radius(R/Ro), Spectral Class, Color, and Absolute Magnitude')
 
-st.subheader('Stars: Temperature (K), Luminosity(L/Lo), Radius(R/Ro)')
-st.text('This dataframe and chart displays the temperature, luminosity, and radii of different stars')
-st.dataframe(df_temp)
-st.bar_chart(df_temp)
 
-st.subheader('Stars: Spectral Class, Color, and Absolute Magnitude')
-st.text('This dataframe and chart displays the spectral class, color, and absolute magnitude of different stars')
-st.dataframe(df_class)
-st.line_chart(df_class)
+# load in data
+df = pd.read_csv('https://raw.githubusercontent.com/zf81/dashboard-streamlit/main/data/Stars.csv')
+df = df.head(100)
+
+
+## Header ##
+st.header('Star Features')
+st.subheader('Spectral Class, Color, Luminosity, Absolute Magnitude')
+## Caption ##
+st.caption('This dataset includes the various features of stars that are utilizied in the  Hertzsprung-Russell diagram (HR diagram), which is one of the most important tools in the study of stellar evolution.')
+
+## Toggle ##
+if st.checkbox('Show first 1000 records of Stars dataset'):
+    st.dataframe(df)
+
+## Code Block  ##
+code= '''def scroll(): print("Scroll Down To See Charts")'''
+st.code(code, language='python')
+
+## Dataframe and Chart 1: Star Temperature and Luminosity  ##
+st.subheader('Temperature of Stars Against Their Luminosity ')
+
+stars = pd.read_csv('https://raw.githubusercontent.com/zf81/dashboard-streamlit/main/data/Stars.csv')
+
+temperature = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["temperature", "luminosity"])
+
+st.bar_chart(temperature)
+
+
+
+################################################################
+## Dataframe and Chart 1: BMI Levels ##
+st.subheader('BMI Levels Among Patients')
+
+diabetes = pd.read_csv('diabetes.csv')
+
+BMI = diabetes['BMI'].value_counts()
+
+st.bar_chart(BMI)
+
+st.caption("This bar chart illustrates the count of unique BMI levels among female patients.")
+
+## DataFrame and Chart 2: Vitals Among Patients ##
+st.subheader('Vitals Levels Among Patients')
+
+chart_data = pd.DataFrame(np.random.randn(20, 4), columns=['Glucose','Insulin','BloodPressure', 'Pregnancies'])
+
+st.line_chart(chart_data)
+
+st.caption("This line chart illustrates to vitals levels of female patients used as diagnostic measurements to predict diabetes.")
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
